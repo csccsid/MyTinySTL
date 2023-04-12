3613edbe4c70cc3e335cbeb526a989296b096c76
csccsid <11510765@mail.sustech.edu.cn>
2023-04-11 20:45:49 -0400

    vector_init

---
+++ MyTinySTL/vector.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef MYTINYSTL_VECTOR_H_
+#define MYTINYSTL_VECTOR_H_
+
+#include <initializer_list>
+#include "iterator.h"
+#include "memory.h"
+#include "util.h"
+#include "exceptdef.h"
+#include "algo.h"
+
+namespace mystl
+{
+} // namespace mystl
+#endif // !MYTINYSTL_VECTOR_H_
