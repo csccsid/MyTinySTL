6203f206754fd638787cdbad0dc8c6753d036dbc
csccsid <11510765@mail.sustech.edu.cn>
2023-04-21 23:59:43 -0400

    constructor and destructor 1

--- MyTinySTL/vector.h
+++ MyTinySTL/vector.h
@@ -26,10 +26,62 @@ template <class T>
 class vector
 {
 	static_assert(!std::is_same<bool, T>::value, "vector<bool> is abandoned in mystl");
+
+private:
+    iterator begin_;  // head of current using space
+    iterator end_;    // tail of current using space 
+    iterator cap_;    // tail of current store space
+
+
 public:
-	// 
+	// constructor and destructor
+    vector() noexcept
+    {
+        try_init();
+    }
+
+    explicit vector(size_type n)
+    {
+        fill_init(n, value_type());
+    }
+
+    vector(size_type n, const value_type& value)
+    {
+        fill_init(n, value);
+    }
+
+
     vector& operator=(const vector& rhs);
     vector& operator=(vector&& rhs) noexcept;
+
+    vector& operator=(std::initializer_list<value_type> ilist)
+    {
+        vector tmp(ilist.begin(), ilist.end());
+        swap(tmp);
+        return *this;
+    }
+
+    ~vector()
+    {
+        destroy_and_recover(begin_, end_, cap_ - begin_);
+        begin_ = end_ = cap_ = nullptr;
+    }
+
+
+
+private:
+    // util functions
+    
+    // initialize / destroy
+    void      try_init() noexcept;
+
+    void      init_space(size_type size, size_type cap);
+
+    void      fill_init(size_type n, const value_type& value);
+    template <class Iter>
+    void      range_init(Iter first, Iter last);
+
+    void      destroy_and_recover(iterator first, iterator last, size_type n);
 };
 
 
